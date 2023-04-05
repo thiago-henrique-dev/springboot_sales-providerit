cacfab6d61a8fbcffa5bfd6e973d167a26501076
thiagohenriquedev <thiagohenriquedev@gmail.com>
2023-04-05 09:10:14 -0300

    initial project

---
+++ src/test/java/com/project/vendas/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.vendas.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
