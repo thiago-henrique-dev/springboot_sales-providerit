7f07e275a6b535471ea514932b3b2eb8334f89df
thiagohenriquedev <thiagohenriquedev@gmail.com>
2023-04-06 14:30:17 -0300

    connection database h2

--- src/main/java/com/project/vendas/MinhaConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.project.vendas;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
-
-@Configuration
-public class MinhaConfiguration {
-   
-}
--- src/main/java/com/project/vendas/VendasApplication.java
+++ src/main/java/com/project/vendas/VendasApplication.java
@@ -1,31 +1,18 @@
 package com.project.vendas;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.project.vendas.repository",
-				"com.project.vendas.repository"})
+
 @RestController
 public class VendasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
-	}
-
-	@Value("${application.name}")
-	private String applicationName;
-
-
-	@GetMapping("/hello")
-	public String helloWorld(){
-		return applicationName;
+		System.out.println("Hello");
 	}
 
 }
