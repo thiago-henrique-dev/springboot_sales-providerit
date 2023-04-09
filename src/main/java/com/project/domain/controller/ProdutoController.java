30e51aece9f900fb085147571490777536a0e2de
thiagohenriquedev <thiagohenriquedev@gmail.com>
2023-04-09 19:06:27 -0300

    edit controller Produto

--- src/main/java/com/project/domain/controller/ProdutoController.java
+++ src/main/java/com/project/domain/controller/ProdutoController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,7 @@ public class ProdutoController {
                         "Produto não encontrado."));
     }
 
+    @DeleteMapping("{id}")
     public void delete(@PathVariable Integer id){
         repository
             .findById(id)
