f7efb45045f6e3b24569de315533db31b3672c3f
thiagohenriquedev <thiagohenriquedev@gmail.com>
2023-04-09 12:05:27 -0300

    POST, GET and Request Body requests

--- src/main/java/com/project/domain/controller/ClienteController.java
+++ src/main/java/com/project/domain/controller/ClienteController.java
@@ -1,32 +1,53 @@
 package com.project.domain.controller;
 
+import com.project.domain.entity.Cliente;
+import com.project.domain.repository.Clientes;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import com.project.domain.entity.Cliente;
-import com.project.domain.repository.Clientes;
-import java.util.Optional;
-
 @Controller
 public class ClienteController {
-    
-    private Clientes clientes;
 
-    public ClienteController( Clientes clientes ) {
-        this.clientes = clientes;
+  private Clientes clientes;
+
+  public ClienteController(Clientes clientes) {
+    this.clientes = clientes;
+  }
+
+  @GetMapping("/api/clientes/{id}")
+  @ResponseBody
+  public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+    Optional<Cliente> cliente = clientes.findById(id);
+
+    if (cliente.isPresent()) {
+      return ResponseEntity.ok(cliente.get());
     }
 
-    @GetMapping("/api/clientes/{id}")
+    return ResponseEntity.notFound().build();
+  }
+
+  @PostMapping("/api/clientes")
+  @ResponseBody
+  public ResponseEntity<Cliente> save(@RequestBody Cliente cliente) {
+    Cliente clienteSalvo = clientes.save(cliente);
+    return ResponseEntity.ok(clienteSalvo);
+  }
+
+  @GetMapping("/api/clientes")
     @ResponseBody
-    public ResponseEntity<Cliente> getClienteById( @PathVariable Integer id ){
-        Optional<Cliente> cliente = clientes.findById(id);
+    public ResponseEntity<List<Cliente>> getTodosClientes() {
+    List<Cliente> clientes = this.clientes.findAll();
 
-        if(cliente.isPresent()){
-            return ResponseEntity.ok( cliente.get() );
-        }
-
-        return ResponseEntity.notFound().build();
+    if (clientes.isEmpty()) {
+        return ResponseEntity.noContent().build();
     }
+
+    return ResponseEntity.ok(clientes);
+    }
+
 }
--- src/main/java/com/project/domain/repository/Clientes.java
+++ src/main/java/com/project/domain/repository/Clientes.java
@@ -26,5 +26,6 @@ public interface Clientes extends JpaRepository<Cliente, Integer> {
     boolean existsByNome(String nome);
     @Query(" select c from Cliente c left join fetch c.pedidos where c.id = :id  ")
     Cliente findClienteFetchPedidos( @Param("id") Integer id );
+
         
 }
