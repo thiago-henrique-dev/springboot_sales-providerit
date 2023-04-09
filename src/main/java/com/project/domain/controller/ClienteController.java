7c2f8a5b30eb7fc7a06e46804026f628fd03ef77
thiagohenriquedev <thiagohenriquedev@gmail.com>
2023-04-09 12:17:48 -0300

    Delete Mapping: deleting a resource on the server

--- src/main/java/com/project/domain/controller/ClienteController.java
+++ src/main/java/com/project/domain/controller/ClienteController.java
@@ -4,7 +4,6 @@ import com.project.domain.entity.Cliente;
 import com.project.domain.repository.Clientes;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -39,15 +38,27 @@ public class ClienteController {
   }
 
   @GetMapping("/api/clientes")
-    @ResponseBody
-    public ResponseEntity<List<Cliente>> getTodosClientes() {
+  @ResponseBody
+  public ResponseEntity<List<Cliente>> getTodosClientes() {
     List<Cliente> clientes = this.clientes.findAll();
 
     if (clientes.isEmpty()) {
-        return ResponseEntity.noContent().build();
+      return ResponseEntity.noContent().build();
     }
 
     return ResponseEntity.ok(clientes);
+  }
+
+  @DeleteMapping("/api/clientes/{id}")
+  @ResponseBody
+  public ResponseEntity<Cliente> delete(@PathVariable Integer id) {
+    Optional<Cliente> cliente = this.clientes.findById(id);
+
+    if (cliente.isPresent()) {
+      clientes.delete(cliente.get());
+      return ResponseEntity.noContent().build();
     }
 
+    return ResponseEntity.notFound().build();
+  }
 }
